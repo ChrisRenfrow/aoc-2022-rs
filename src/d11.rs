56e91bba52a60041c856cdb8eaa12ddda212787c
Chris Renfrow <dev@chrisrenfrow.me>
2022-12-12 19:49:42 -0800

    WIP: Doesn't compile, ownership & mutability issues

--- src/d11.rs
+++ src/d11.rs
@@ -1,3 +1,7 @@
+use std::cell::Cell;
+use std::collections::VecDeque;
+use std::fmt::{Debug, Formatter, Result as FmtResult};
+use std::rc::Rc;
 use std::str::FromStr;
 
 #[aoc_generator(day11)]
@@ -10,41 +14,113 @@ fn parse_input(input: &str) -> Vec<Monkey> {
 
 #[aoc(day11, part1)]
 fn solve_d11_pt1(monkeys: &Vec<Monkey>) -> u32 {
+    dbg!(&monkeys);
     todo!()
+}
+
+struct Game {
+    monkeys: Vec<Rc<Cell<Monkey>>>,
+}
+
+impl Game {
+    fn run_for(&mut self, rounds: usize) {
+        for r in 0..rounds {
+            self.round();
+        }
+    }
+
+    fn round(&self) {
+        self.monkeys.iter().map(|m| {
+            let thrown = *m.into_inner().turn().to_vec();
+        });
+    }
+
+    fn top_n_monkey_biz(&self, n: usize) -> usize {
+        let mut sorted = self
+            .monkeys
+            .iter()
+            .map(|m| m.into_inner().activity)
+            .collect::<Vec<usize>>();
+        sorted.sort();
+        sorted.iter().take(n).fold(0, |acc, a| acc + a)
+    }
 }
 
 type Item = u32;
 
 /// Monkeh
-// #[derive(Debug)]
 struct Monkey {
     /// The id of the monkey as defined by the input.
     id: usize,
     /// A list of items the monkey starts with.
-    start: Vec<Item>,
+    items: VecDeque<Item>,
     /// The operation to apply to the currently held item.
-    op: Box<dyn Fn(Option<Item>) -> u32>,
+    op: Box<dyn Fn(Item) -> u32>,
     /// The function used to decide which monkey to throw to.
     test: Box<dyn Fn(Item) -> bool>,
     /// The IDs of the monkeys the item will be thrown to if test evaluates true or false, respectively.
     throw: (usize, usize),
+    /// Activity count, how many times the monkey has inspected an item over all rounds
+    activity: usize,
 }
 
 impl Monkey {
     fn new(
         id: usize,
-        start: Vec<u32>,
-        op: Box<dyn Fn(Option<u32>) -> u32>,
+        items: VecDeque<u32>,
+        op: Box<dyn Fn(u32) -> u32>,
         test: Box<dyn Fn(u32) -> bool>,
         throw: (usize, usize),
     ) -> Self {
         Self {
             id,
-            start,
+            items,
             op,
             test,
             throw,
+            activity: 0,
         }
+    }
+
+    fn turn(&mut self) -> Vec<(usize, Item)> {
+        let mut thrown_items: Vec<(usize, Item)> = vec![];
+
+        while let Some(item) = self.inspect_next() {
+            thrown_items.push(item);
+        }
+
+        thrown_items
+    }
+
+    fn inspect_next(&mut self) -> Option<(usize, Item)> {
+        let mut items = &mut self.items;
+        let item = items.pop_front();
+        if item.is_none() {
+            return None;
+        }
+        let item = (self.op)(item.unwrap());
+        self.activity += 1;
+        match (self.test)(item) {
+            true => Some((self.throw.0, item)),
+            false => Some((self.throw.1, item)),
+        }
+    }
+
+    fn catch_item(&self, incoming: Item) {
+        let mut items = &mut self.items;
+        items.push_back(incoming);
+    }
+}
+
+impl Debug for Monkey {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        f.debug_struct("Monkey")
+            .field("id", &self.id)
+            .field("items", &self.items)
+            .field("op", &"Box<dyn Fn(Item) -> u32>")
+            .field("test", &"Box<dyn Fn(Item) -> bool>")
+            .field("throw", &self.throw)
+            .finish()
     }
 }
 
@@ -66,7 +142,7 @@ impl FromStr for Monkey {
         let start = parse_start(lines.next().unwrap());
         let op = parse_op(lines.next().unwrap());
         let test = parse_test(lines.next().unwrap());
-        let throw = parse_throw(lines.next().unwrap());
+        let throw = parse_throw(lines.next().unwrap(), lines.next().unwrap());
 
         Ok(Monkey::new(id, start, op, test, throw))
     }
@@ -83,15 +159,15 @@ fn parse_id(s: &str) -> usize {
         .unwrap()
 }
 
-fn parse_start(s: &str) -> Vec<u32> {
+fn parse_start(s: &str) -> VecDeque<Item> {
     s.split(":").collect::<Vec<&str>>()[1]
         .trim()
         .split(", ")
-        .map(|n| n.parse::<u32>().unwrap())
+        .map(|n| n.parse::<Item>().unwrap())
         .collect()
 }
 
-fn parse_op(s: &str) -> Box<dyn Fn(Option<u32>) -> u32> {
+fn parse_op(s: &str) -> Box<dyn Fn(u32) -> u32> {
     let mut op_str = s
         .split(":")
         .nth(1)
@@ -106,21 +182,46 @@ fn parse_op(s: &str) -> Box<dyn Fn(Option<u32>) -> u32> {
         "+" => Op::Add,
         _ => unreachable!(),
     };
-    let b = op_str.next().unwrap().parse::<u32>().unwrap();
+    // dbg!(&op_str);
+    let b = op_str.next().unwrap().parse::<u32>();
 
     match op {
-        Op::Mul => Box::new(move |a| a.unwrap_or(b) * b),
-        Op::Add => Box::new(move |a| a.unwrap_or(b) + b),
+        Op::Mul => Box::new(move |a| a * b.as_ref().unwrap_or(&a)),
+        Op::Add => Box::new(move |a| a + b.as_ref().unwrap_or(&a)),
     }
 }
 
 fn parse_test(s: &str) -> Box<dyn Fn(u32) -> bool> {
-    todo!()
+    let b = s
+        .split(":")
+        .nth(1)
+        .unwrap()
+        .trim()
+        .split_whitespace()
+        .nth(2)
+        .unwrap()
+        .parse::<u32>()
+        .unwrap();
+    Box::new(move |a| a % b == 0)
 }
 
 // returns (if_true_id, if_false_id)
-fn parse_throw(s: &str) -> (usize, usize) {
-    todo!()
+fn parse_throw(s1: &str, s2: &str) -> (usize, usize) {
+    let t = s1
+        .trim()
+        .split_whitespace()
+        .nth(5)
+        .unwrap()
+        .parse::<usize>()
+        .unwrap();
+    let f = s2
+        .trim()
+        .split_whitespace()
+        .nth(5)
+        .unwrap()
+        .parse::<usize>()
+        .unwrap();
+    (t, f)
 }
 
 mod tests {
