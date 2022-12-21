d1d7e59067f2912423f9849cb42bdc5c89597df1
Chris Renfrow <dev@chrisrenfrow.me>
2022-12-20 19:22:54 -0800

    WIP: Revisit d05 to re-learn nom

--- src/d05.rs
+++ src/d05.rs
@@ -1,112 +1,93 @@
 use std::collections::VecDeque;
 use std::num::ParseIntError;
-use std::result::Result;
 use std::str::FromStr;
 
-type Stack = VecDeque<Crate>;
-type Crate = char;
+#[aoc_generator(day5)]
+fn parse_input(input: &str) -> (Vec<Move>, Vec<Stack>) {
+    let parts = input.trim().split("\n\n").collect::<Vec<&str>>();
+    let moves = parts[1]
+        .lines()
+        .map(parser::parse_move)
+        .flatten()
+        .map(|(_, m)| m)
+        .collect::<Vec<Move>>();
 
-#[derive(Debug)]
-struct Depot {
-    stacks: Vec<Stack>,
-    orders: Vec<Order>,
+    dbg!(&moves);
+
+    let stack_rows = parts[0]
+        .lines()
+        .map(parser::parse_stack_row)
+        .flatten()
+        .map(|(_, r)| r)
+        .collect::<Vec<Vec<Option<Crate>>>>();
+
+    dbg!(&stack_rows);
+
+    // recreate rows as columns
+    let stacks = todo!();
+
+    dbg!(&stacks);
+
+    (moves, stacks)
 }
 
-#[derive(Debug, Copy, Clone)]
-struct Order {
+#[aoc(day5, part1)]
+fn solve_d05_pt1(input: &(Vec<Move>, Vec<Stack>)) -> String {
+    let (moves, stacks) = input;
+    todo!()
+}
+
+pub type Stack = VecDeque<Option<Crate>>;
+
+#[derive(Debug)]
+pub struct Crate {
+    label: char,
+}
+
+#[derive(Debug)]
+pub struct Move {
     qty: usize,
     from: usize,
     to: usize,
 }
 
-struct ParseOrderError;
-
-fn parse_order(s: &str) -> Result<Order, ParseIntError> {
-    let mut order_iter = s.trim().split(' ');
-    Ok(Order {
-        qty: order_iter.nth(1).unwrap().parse()?,
-        from: order_iter.nth(1).unwrap().parse()?,
-        to: order_iter.nth(1).unwrap().parse()?,
-    })
-}
-
-impl FromStr for Order {
-    type Err = ParseIntError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        parse_order(&s)
+impl Move {
+    pub fn new(qty: usize, from: usize, to: usize) -> Self {
+        Self { qty, from, to }
     }
 }
 
-impl Depot {
-    pub fn take_qty_from(mut self, qty: usize, stack: usize) -> Stack {
-        let mut from = VecDeque::new();
-        for n in 0..qty {
-            let c = self.stacks[stack].pop_front().unwrap();
-            from.push_front(c);
-        }
+mod parser {
+    use super::{Crate, Move};
+    use nom::{bytes::complete::tag, character::complete::digit1, combinator::map_res, IResult};
 
-        from
+    pub fn parse_move(input: &str) -> IResult<&str, Move> {
+        let (input, qty) = field("move ", input)?;
+        let (input, from) = field(" from ", input)?;
+        let (input, to) = field(" to ", input)?;
+        Ok((input, Move::new(qty, from, to)))
     }
 
-    pub fn add_crates_to(mut self, stack: usize, crates: Stack) {
-        for c in crates {
-            self.stacks[stack].push_front(c);
-        }
+    fn field<'a>(field: &'a str, input: &'a str) -> IResult<&'a str, usize> {
+        // TODO: consume surrounding whitespace before returning
+        let (input, _) = tag(field)(input)?;
+        map_res(digit1, number)(input)
     }
 
-    pub fn top_crates(&self) -> Vec<Crate> {
-        self.stacks.iter().map(|s| s[0]).collect::<Vec<Crate>>()
-    }
-}
-
-/// Particularly brittle parsing solution but good enough for now
-fn parse_stacks(s: &str) -> Vec<Stack> {
-    let stack_rows: Vec<Vec<char>> = s
-        .lines()
-        .map(|l| l[1..].chars().step_by(4).collect())
-        .collect();
-
-    // Turn rows into columns
-    let num_stacks = stack_rows[0].len();
-    let mut stacks: Vec<Stack> = vec![];
-
-    for s in 0..num_stacks {
-        stacks.push(VecDeque::new());
-        for r in &stack_rows {
-            if r[s].is_numeric() {
-                break;
-            } else if r[s].is_alphabetic() {
-                stacks[s].push_front(r[s]);
-            }
-        }
+    fn number(input: &str) -> Result<usize, std::num::ParseIntError> {
+        input.parse()
     }
 
-    stacks
-}
-
-#[aoc_generator(day5)]
-fn parse_input(input: &str) -> Depot {
-    let (stack_str, orders_str) = input.split_once("\n\n").unwrap();
-    let orders = orders_str
-        .lines()
-        .map(Order::from_str)
-        .flatten()
-        .collect::<Vec<Order>>();
-    let stacks = parse_stacks(stack_str);
-
-    // dbg!(&orders, &stacks);
-    Depot { orders, stacks }
-}
-
-#[aoc(day5, part1)]
-fn solve_d05_pt1(d: &Depot) -> String {
-    dbg!(&d);
-    for o in &d.orders {
-        let from = &d.take_qty_from(o.qty, o.from);
-        d.add_crates_to(o.to, *from);
+    pub fn parse_stack_row(input: &str) -> IResult<&str, Vec<Option<Crate>>> {
+        // crates in a row are chunks of 3 bytes followed by a single space
+        todo!()
     }
-    d.top_crates().iter().cloned().collect::<String>()
+
+    fn parse_crate(input: &str) -> IResult<&str, Option<Crate>> {
+        // "    " -> None
+        // "[A] " -> Some(Crate { label: 'A' })
+        todo!()
+    }
 }
 
 #[cfg(test)]
