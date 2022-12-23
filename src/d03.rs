3e6d5429426f2117970578c5ab9ab093c9810a27
Chris Renfrow <dev@chrisrenfrow.me>
2022-12-22 16:32:22 -0800

    Revisit d03, solve pt2

--- src/d03.rs
+++ src/d03.rs
@@ -1,5 +1,12 @@
+use hashbrown::HashSet;
+
 type Sack = (Vec<Item>, Vec<Item>);
 type Item = char;
+
+#[aoc_generator(day3, part1)]
+fn parse_input_pt1(input: &str) -> Vec<Sack> {
+    input.trim().lines().map(line_to_sack).collect()
+}
 
 fn line_to_sack(line: &str) -> Sack {
     let length = line.len();
@@ -7,15 +14,15 @@ fn line_to_sack(line: &str) -> Sack {
     (chars[0..length / 2].to_vec(), chars[length / 2..].to_vec())
 }
 
-#[aoc_generator(day3)]
-fn input_generator(input: &str) -> Vec<Sack> {
-    input.trim().lines().map(line_to_sack).collect()
+#[aoc(day3, part1)]
+fn solve_d03_pt1(sacks: &[Sack]) -> u32 {
+    sacks.iter().map(|s| item_priority(common_item(s))).sum()
 }
 
 fn common_item(sack: &Sack) -> Item {
     let (comp_a, comp_b) = sack;
     for item in comp_a {
-        if comp_b.contains(&item) {
+        if comp_b.contains(item) {
             return *item;
         }
     }
@@ -31,9 +38,34 @@ fn item_priority(item: Item) -> u32 {
     }
 }
 
-#[aoc(day3, part1)]
-fn solve_d03_pt1<'a>(sacks: &'a Vec<Sack>) -> u32 {
-    sacks.iter().map(|s| item_priority(common_item(s))).sum()
+#[aoc_generator(day3, part2)]
+fn parse_input_pt2(input: &str) -> Vec<String> {
+    input.trim().lines().map(str::to_string).collect()
+}
+
+#[aoc(day3, part2)]
+fn solve_d03_pt2(sacks: &[String]) -> u32 {
+    sacks.chunks(3).map(group_value).sum()
+}
+
+fn group_value(sacks: &[String]) -> u32 {
+    item_priority(group_badge(sacks))
+}
+
+fn group_badge(sacks: &[String]) -> Item {
+    let sets: Vec<HashSet<char>> = sacks.iter().map(|s| s.chars().collect()).collect();
+
+    sets.iter()
+        .fold(HashSet::new(), |common, s| {
+            if common.is_empty() {
+                s.iter().copied().collect()
+            } else {
+                common.intersection(s).copied().collect()
+            }
+        })
+        .drain()
+        .next()
+        .unwrap()
 }
 
 #[cfg(test)]
@@ -50,7 +82,7 @@ CrZsJsPPZsGzwwsLwLmpwMDw";
 
     #[test]
     fn example_pt1() {
-        let input = input_generator(EXAMPLE_INPUT);
+        let input = parse_input_pt1(EXAMPLE_INPUT);
         let expect = 157;
         let actual = solve_d03_pt1(&input);
 
@@ -59,9 +91,27 @@ CrZsJsPPZsGzwwsLwLmpwMDw";
 
     #[test]
     fn solve_input_pt1() {
-        let input = input_generator(FILE_INPUT);
+        let input = parse_input_pt1(FILE_INPUT);
         let expect = 7848;
         let actual = solve_d03_pt1(&input);
+
+        assert_eq!(expect, actual);
+    }
+
+    #[test]
+    fn example_pt2() {
+        let input = parse_input_pt2(EXAMPLE_INPUT);
+        let expect = 70;
+        let actual = solve_d03_pt2(&input);
+
+        assert_eq!(expect, actual);
+    }
+
+    #[test]
+    fn solve_input_pt2() {
+        let input = parse_input_pt2(FILE_INPUT);
+        let expect = 2616;
+        let actual = solve_d03_pt2(&input);
 
         assert_eq!(expect, actual);
     }
