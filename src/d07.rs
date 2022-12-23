e9af7893cc43d2513f1c2d4ed7f13aca83123924
Chris Renfrow <dev@chrisrenfrow.me>
2022-12-23 14:28:05 -0800

    WIP: Revisit d07, use nom for parsing

--- src/d07.rs
+++ src/d07.rs
@@ -1,75 +1,70 @@
-use std::collections::HashMap;
-
-use itertools::Itertools;
-
-#[derive(Debug)]
-enum Output {
-    Command(Cmd),
-    Out(Item),
-}
-
-#[derive(Debug)]
-enum Cmd {
-    Cd(String),
-    Ls,
-}
-
-#[derive(Debug)]
-enum Item {
-    Dir,
-    File(usize),
-}
-
-// Brittle parsing for now
-fn parse_line(line: &str) -> Output {
-    let splt: Vec<&str> = line.trim().split(' ').collect();
-    if splt[0] == "$" {
-        dbg!(line);
-        Output::Command(match splt[1] {
-            "cd" => Cmd::Cd(splt[2].to_string()),
-            "ls" => Cmd::Ls,
-            &_ => unreachable!(),
-        })
-    } else {
-        Output::Out(match splt[0] {
-            "dir" => Item::Dir,
-            _ => Item::File(splt[0].parse().unwrap()),
-        })
-    }
-}
+use parser::Line;
 
 #[aoc_generator(day7)]
-fn parse_input(input: &str) -> Vec<Output> {
-    input.trim().lines().map(parse_line).collect()
-}
-
-fn sum_dirs_below_x(dirs: Vec<(String, usize)>, x: usize) -> usize {
-    dirs.iter()
-        .filter(|(_, size)| size <= &x)
-        .fold(0, |acc, (_, size)| acc + size)
+fn parse_input(input: &str) -> Vec<Line> {
+    input
+        .trim()
+        .lines()
+        .flat_map(parser::output_line)
+        .map(|(_, l)| l)
+        .collect()
 }
 
 #[aoc(day7, part1)]
-fn solve_d07_pt1(output: &Vec<Output>) -> usize {
-    let mut dir_totals = HashMap::new();
+fn solve_d07_pt1(output: &[Line]) -> usize {
+    dbg!(&output);
+    todo!()
+}
 
-    let mut curr_dir = "".to_string();
+mod parser {
 
-    for o in output {
-        match o {
-            Output::Command(Cmd::Cd(d)) => curr_dir = d.clone(),
-            Output::Out(Item::File(s)) => {
-                if let Some(d) = dir_totals.get_mut(&curr_dir) {
-                    *d = *d + s;
-                } else {
-                    dir_totals.insert(curr_dir.clone(), *s);
-                }
-            }
-            _ => (),
-        }
+    use nom::{
+        branch::alt,
+        bytes::complete::tag,
+        character::complete::{alpha1, char, digit1, space1},
+        combinator::{rest, value},
+        sequence::{pair, preceded, terminated},
+        IResult,
+    };
+
+    pub fn output_line(i: &str) -> IResult<&str, Line> {
+        alt((cmd, file))(i)
     }
 
-    sum_dirs_below_x(dir_totals.drain().collect(), 100000)
+    fn cmd(i: &str) -> IResult<&str, Line> {
+        preceded(pair(char('$'), space1), alt((ls, cd)))(i)
+    }
+
+    fn ls(i: &str) -> IResult<&str, Line> {
+        value(Line::CmdLs, tag("ls"))(i)
+    }
+
+    fn cd(i: &str) -> IResult<&str, Line> {
+        let (i, arg) = preceded(pair(tag("cd"), space1), rest)(i)?;
+        Ok((i, Line::CmdCd(arg.to_string())))
+    }
+
+    fn file(i: &str) -> IResult<&str, Line> {
+        alt((plain, dir))(i)
+    }
+
+    fn plain(i: &str) -> IResult<&str, Line> {
+        let (i, size_str) = terminated(digit1, pair(space1, rest))(i)?;
+        Ok((i, Line::File(size_str.parse::<usize>().unwrap())))
+    }
+
+    fn dir(i: &str) -> IResult<&str, Line> {
+        let (i, label) = preceded(pair(tag("dir"), space1), alpha1)(i)?;
+        Ok((i, Line::Dir(label.to_string())))
+    }
+
+    #[derive(Debug, Clone)]
+    pub enum Line {
+        CmdLs,
+        CmdCd(String),
+        File(usize),
+        Dir(String),
+    }
 }
 
 #[cfg(test)]
@@ -117,21 +112,21 @@ $ ls
         assert_eq!(expect, actual);
     }
 
-    #[ignore]
-    #[test]
-    fn example_pt2() {
-        let input = parse_input(EXAMPLE_INPUT);
-        let expect = 0;
-        let actual = solve_d07_pt1(&input);
-        assert_eq!(expect, actual);
-    }
+    // #[ignore]
+    // #[test]
+    // fn example_pt2() {
+    //     let input = parse_input(EXAMPLE_INPUT);
+    //     let expect = 0;
+    //     let actual = solve_d07_pt1(&input);
+    //     assert_eq!(expect, actual);
+    // }
 
-    #[ignore]
-    #[test]
-    fn solve_pt2() {
-        let input = parse_input(FILE_INPUT);
-        let expect = 0;
-        let actual = solve_d07_pt1(&input);
-        assert_eq!(expect, actual);
-    }
+    // #[ignore]
+    // #[test]
+    // fn solve_pt2() {
+    //     let input = parse_input(FILE_INPUT);
+    //     let expect = 0;
+    //     let actual = solve_d07_pt1(&input);
+    //     assert_eq!(expect, actual);
+    // }
 }
